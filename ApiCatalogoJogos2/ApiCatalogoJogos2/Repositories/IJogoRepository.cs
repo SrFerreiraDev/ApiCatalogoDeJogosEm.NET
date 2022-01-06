7b6b8d0a2cd628b1e875dea63813e16f64515d36
Ferreira <93156140+SrFerreiraDev@users.noreply.github.com>
2022-01-05 22:43:57 -0300

    Add files via upload

---
+++ ApiCatalogoJogos2/ApiCatalogoJogos2/Repositories/IJogoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiCatalogoJogos2.Entities;
+
+namespace ApiCatalogoJogos2.Repositories
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
